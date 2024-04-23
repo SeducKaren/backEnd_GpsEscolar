bc6eee651fd2d1f36fb63c3dc0d6174ed194c36d
Janielson Anjos <jan.tablet.uni@gmail.com>
2024-04-22 22:46:46 -0300

    feat: Correção tipo number escola

--- src/controllers/EscolaController.ts
+++ src/controllers/EscolaController.ts
@@ -7,10 +7,26 @@ import EscolaModel from "../models/escolaModel";
 
 class EscolaContoller {
 
+    // static async getAllEscolas(req: Request, res: Response): Promise<void> {
+    //     try {
+    //         const escolas = await EscolaModel.findAll();
+    //         res.status(200).json(escolas);
+    //     } catch (error) {
+    //         console.error(error);
+    //         res.status(500).json({ message: "internal server error"});
+            
+    //     }
+    // }
     static async getAllEscolas(req: Request, res: Response): Promise<void> {
         try {
             const escolas = await EscolaModel.findAll();
-            res.status(200).json(escolas);
+            const simplifiedEscolas = escolas.map(escola => ({
+                id_escola: escola.id_escola,
+                nome_escola: escola.nome_escola,
+                latitude: escola.latitude,
+                longitude: escola.longitude,
+            }));
+            res.status(200).json(simplifiedEscolas);
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: "internal server error"});
--- src/models/escolaModel.ts
+++ src/models/escolaModel.ts
@@ -1,3 +1,47 @@
+// import { Console } from "console";
+// import { Pool } from "pg";
+// import { PoolClient } from "pg";
+
+// interface ExtendedRequest extends Request {
+//     db?: PoolClient
+// }
+
+// class EscolaModel {
+//     static pool = new Pool({
+//         ssl: {
+//             rejectUnauthorized: false,
+//         },
+//         // connectionString: process.env.DATABASE_URL || "postgres://postgres:bancogps@banco-gps.cnomqqwy21eq.sa-east-1.rds.amazonaws.com/db_gps",
+//         connectionString: process.env.DATABASE_URL || "postgres://postgres:bancogps@banco-gps.cxg6s0ekwvkc.us-east-1.rds.amazonaws.com:5432/db_gps",
+//         // connectionString: "postgres://postgres:bancogps@banco-gps.cxg6s0ekwvkc.us-east-1.rds.amazonaws.com",
+        
+//     });
+    
+    
+//     id_escola: number;
+//     nome_escola: string;
+//     latitude: string;
+//     longitude: string;
+
+//     constructor(data:any) {
+//         this.id_escola = data.id_escola || undefined;
+//         this.nome_escola = data.nome_escola || undefined;
+//         this.latitude = data.latitude || undefined;
+//         this.longitude = data.longitude || undefined;
+//     }
+
+
+
+//     static async findAll(): Promise<EscolaModel[]> {
+//         // const result = await this.pool.query('SELECT * FROM escola');
+//         const result = await this.pool.query('SELECT * FROM escola');
+//         return result.rows.map((data: any) => new EscolaModel(data));
+        
+//     }
+// }
+
+// export default EscolaModel;
+
 import { Console } from "console";
 import { Pool } from "pg";
 import { PoolClient } from "pg";
@@ -20,14 +64,14 @@ class EscolaModel {
     
     id_escola: number;
     nome_escola: string;
-    latitude: string;
-    longitude: string;
+    latitude: number;
+    longitude: number;
 
     constructor(data:any) {
         this.id_escola = data.id_escola || undefined;
         this.nome_escola = data.nome_escola || undefined;
-        this.latitude = data.latitude || undefined;
-        this.longitude = data.longitude || undefined;
+        this.latitude = parseFloat(data.latitude);
+        this.longitude = parseFloat(data.longitude);
     }
 
 
