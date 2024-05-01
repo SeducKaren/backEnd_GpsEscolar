fe1a14c208efc521b5be1706adb575b1cf6b5e20
Janielson Anjos <jan.tablet.uni@gmail.com>
2024-05-01 03:25:55 -0300

    feat: Add Paradas das rotas

--- src/controllers/ParadaController.ts
+++ src/controllers/ParadaController.ts
@@ -92,5 +92,156 @@ class ParadaController {
             res.status(500).json({ message: "Internal server error" });
         }
     }
+
+    static async getAllParadasRota04ida(req: Request, res: Response): Promise<void> {
+        try {
+            const paradas = await ParadaModel.findAllRota04ida();
+            res.status(200).json(paradas);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Internal server error"});
+        }
+    }
+
+
+    static async getParadaByIdRota04ida(req: Request, res: Response): Promise<void> {
+        const paradaId = parseInt(req.params.id);
+        if (isNaN(paradaId)) {
+            res.status(400).json({ message: "ID da parada inválido" });
+            return;
+        }
+    
+        try {
+            const parada = await ParadaModel.findByIdRota04ida(paradaId.toString());
+            if (parada) {
+                res.status(200).json(parada);
+            } else {
+                res.status(404).json({ message: "Parada não encontrada" });
+            }
+        } catch (error) {
+            console.error('Erro ao buscar parada:', error);
+            res.status(500).json({ message: 'Erro interno do servidor' });
+        }
+    }
+    static async getAllParadasRota04volta(req: Request, res: Response): Promise<void> {
+        try {
+            const paradas = await ParadaModel.findAllRota04volta();
+            res.status(200).json(paradas);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Internal server error"});
+        }
+    }
+
+
+    static async getParadaByIdRota04volta(req: Request, res: Response): Promise<void> {
+        const paradaId = parseInt(req.params.id);
+        if (isNaN(paradaId)) {
+            res.status(400).json({ message: "ID da parada inválido" });
+            return;
+        }
+    
+        try {
+            const parada = await ParadaModel.findByIdRota04volta(paradaId.toString());
+            if (parada) {
+                res.status(200).json(parada);
+            } else {
+                res.status(404).json({ message: "Parada não encontrada" });
+            }
+        } catch (error) {
+            console.error('Erro ao buscar parada:', error);
+            res.status(500).json({ message: 'Erro interno do servidor' });
+        }
+    }
+    static async getAllParadasRota05ida(req: Request, res: Response): Promise<void> {
+        try {
+            const paradas = await ParadaModel.findAllRota05ida();
+            res.status(200).json(paradas);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Internal server error"});
+        }
+    }
+
+
+    static async getParadaByIdRota05ida(req: Request, res: Response): Promise<void> {
+        const paradaId = parseInt(req.params.id);
+        if (isNaN(paradaId)) {
+            res.status(400).json({ message: "ID da parada inválido" });
+            return;
+        }
+    
+        try {
+            const parada = await ParadaModel.findByIdRota05ida(paradaId.toString());
+            if (parada) {
+                res.status(200).json(parada);
+            } else {
+                res.status(404).json({ message: "Parada não encontrada" });
+            }
+        } catch (error) {
+            console.error('Erro ao buscar parada:', error);
+            res.status(500).json({ message: 'Erro interno do servidor' });
+        }
+    }
+    static async getAllParadasRota05volta(req: Request, res: Response): Promise<void> {
+        try {
+            const paradas = await ParadaModel.findAllRota05volta();
+            res.status(200).json(paradas);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Internal server error"});
+        }
+    }
+
+
+    static async getParadaByIdRota05volta(req: Request, res: Response): Promise<void> {
+        const paradaId = parseInt(req.params.id);
+        if (isNaN(paradaId)) {
+            res.status(400).json({ message: "ID da parada inválido" });
+            return;
+        }
+    
+        try {
+            const parada = await ParadaModel.findByIdRota05volta(paradaId.toString());
+            if (parada) {
+                res.status(200).json(parada);
+            } else {
+                res.status(404).json({ message: "Parada não encontrada" });
+            }
+        } catch (error) {
+            console.error('Erro ao buscar parada:', error);
+            res.status(500).json({ message: 'Erro interno do servidor' });
+        }
+    }
+    static async getAllParadasRota06volta(req: Request, res: Response): Promise<void> {
+        try {
+            const paradas = await ParadaModel.findAllRota06volta();
+            res.status(200).json(paradas);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Internal server error"});
+        }
+    }
+
+
+    static async getParadaByIdRota06volta(req: Request, res: Response): Promise<void> {
+        const paradaId = parseInt(req.params.id);
+        if (isNaN(paradaId)) {
+            res.status(400).json({ message: "ID da parada inválido" });
+            return;
+        }
+    
+        try {
+            const parada = await ParadaModel.findByIdRota06volta(paradaId.toString());
+            if (parada) {
+                res.status(200).json(parada);
+            } else {
+                res.status(404).json({ message: "Parada não encontrada" });
+            }
+        } catch (error) {
+            console.error('Erro ao buscar parada:', error);
+            res.status(500).json({ message: 'Erro interno do servidor' });
+        }
+    }
 }
 export default ParadaController;
--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -5,6 +5,11 @@ import rotasRoutes from './rotasRoutes';
 import loginRoutes from './loginRoutes';
 import paradaRoutes from './paradaRoutes';
 import paradaRota06 from './paradaRota06Routes';
+import paradaRota04Ida from './paradaRota04IdaRoutes';
+import paradaRota04Volta from './paradaRota04VoltaRoutes';
+import paradaRota05Ida from './paradaRota05IdaRoutes';
+import paradaRota05Volta from './paradaRota05VoltaRoutes';
+import paradaRota06Volta from './paradaRota06VoltaRoutes';
 
 
 const indexRouter = express.Router();
@@ -14,6 +19,11 @@ indexRouter.use('/usuario', usuarioRoutes);
 indexRouter.use('/rotas', rotasRoutes);
 indexRouter.use('/login', loginRoutes);
 indexRouter.use('/parada', paradaRoutes);
-indexRouter.use('/rota06', paradaRota06)
+indexRouter.use('/rota06', paradaRota06);
+indexRouter.use('/rota04ida', paradaRota04Ida);
+indexRouter.use('/rota04volta', paradaRota04Volta);
+indexRouter.use('/rota05ida', paradaRota05Ida);
+indexRouter.use('/rota05volta', paradaRota05Volta);
+indexRouter.use('/rota06volta', paradaRota06Volta);
 
 export default indexRouter;
